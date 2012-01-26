7093a144aab09ddcb543f320a56b7249c8cf90fd
Oier Bravo Urtasun <oierbravo@gmail.com>
2012-01-26 14:50:50 +0100

    Clientes para los request

--- nodejs/routes/index.js
+++ nodejs/routes/index.js
@@ -2,7 +2,7 @@
 /*
  * GET home page.
  */
-
+var request = require('request');
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
@@ -15,5 +15,9 @@ exports.arduino = function(req, res){
 
   var message = '[' +  currentTime + ']' + ' ' + 'ID:' + req.params.id + ' AMPS:' + req.params.amp;
   console.log(message);
-  res.render('index', { title: 'Express' })
+  request('http://localhost:4000/arduino/' + req.params.id + '/' + req.params.amp, function(err, req, body){});
+  request('http://localhost:5000/arduino/' + req.params.id + '/' + req.params.amp, function(err, req, body){});
+  request('http://localhost:6000/arduino/' + req.params.id + '/' + req.params.amp, function(err, req, body){});
+
+    res.render('index', { title: 'Express' })
 };
